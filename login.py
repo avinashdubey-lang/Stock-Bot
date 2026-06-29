c20dc04de529118b2251c32b1c0d9793fce86a99
Avinash Dubey <avinashdubey930@gmail.com>
2026-06-29 21:15:41 +0530

    improved login

--- login.py
+++ login.py
@@ -2,30 +2,34 @@
 from SmartApi import SmartConnect
 import pyotp
 import time
-def login_user():
 
-    api_key = "K9Fhvfho"
-    username = "AAAN998226"
-    password = "3027"
 
-    totp_secret = "UA3PJRBKTOTUQSVH67Y4F5ZEZM"
+def login_user(account):
+
+    api_key = account["api_key"]
+    username = account["client_code"]
+    password = account["password"]
+    totp_secret = account["totp_secret"]
 
     totp = pyotp.TOTP(totp_secret).now()
 
     smartApi = SmartConnect(api_key=api_key)
 
     for attempt in range(3):
+
         try:
+
             session = smartApi.generateSession(
                 username,
                 password,
                 totp
             )
+
             break
 
         except Exception as e:
 
-            print(f"⚠️ Login attempt {attempt+1} failed:")
+            print(f"⚠️ Login attempt {attempt + 1} failed for {account['name']}")
             print(e)
 
             if attempt == 2:
@@ -39,14 +43,10 @@ def login_user():
 
     data = session["data"]
 
-    feed_token = data["feedToken"]
-    client_code = data["clientcode"]
-    jwt_token = data["jwtToken"]
-
     return (
-    smartApi,
-    feed_token,
-    client_code,
-    api_key,
-    jwt_token
-)
+        smartApi,
+        data["feedToken"],
+        data["clientcode"],
+        api_key,
+        data["jwtToken"]
+    )
